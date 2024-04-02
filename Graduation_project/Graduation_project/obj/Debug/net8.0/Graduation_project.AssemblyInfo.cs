4e6545dea61b2d25c15729090bf43e3c5a8ddabf
Martina Said <martinasaid1386@gmail.com>
2024-04-02 20:22:23 +0200

    modify all

--- Graduation_project/Graduation_project/obj/Debug/net8.0/Graduation_project.AssemblyInfo.cs
+++ Graduation_project/Graduation_project/obj/Debug/net8.0/Graduation_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graduation_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23f108a0f9d264325b6461fe59e6f2c35eb7927")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5decfaae4d8cbb51518e2b1bf2c117f430e85eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graduation_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graduation_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
