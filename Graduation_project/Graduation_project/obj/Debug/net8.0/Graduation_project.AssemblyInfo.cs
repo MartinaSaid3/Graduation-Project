26ad3b602e1a9277574d9b9e7850add510172d87
nourhan <nourhan@yahoo.com>
2024-04-03 00:46:31 +0200

    venueupdated

--- Graduation_project/Graduation_project/obj/Debug/net8.0/Graduation_project.AssemblyInfo.cs
+++ Graduation_project/Graduation_project/obj/Debug/net8.0/Graduation_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graduation_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1662c69eebe0820ae1bd365a072658f55cb9fc7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffab5cdba01e858d103891f7f7eed5a7adcc23f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graduation_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graduation_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
